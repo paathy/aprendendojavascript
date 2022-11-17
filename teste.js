df6ab34f42cccf4f918212d2dd954ff5e70a46b4
paathy <pathy.limaap@gmail.com>
2022-11-17 18:18:39 -0300

    add comentario

--- teste.js
+++ teste.js
@@ -8,6 +8,42 @@ var nomePessoa = "Pathy";
 console.log(nomePessoa);
 
 
-var n = 19;
-console.log(typeof(n)); 
+var v = 19;
+console.log(typeof(v)); 
 
+// outra aula
+
+var n = 3;
+// n = n * 10;
+n*=10;
+
+console.log(n);
+
+
+var x = 10;
+x--
+
+b= 20;
+c= 60;
+
+console.log(x);
+
+console.log(n <= x && b != c);
+
+console.log(n <= x || b != c);
+
+console.log(!(n <= x || b != c));
+
+// verificar se a é maior que b e a é divisivel por 2
+console.log(a> b && % 2 == 0)
+
+//verificar se b é maior igual a a ou b é divisivel por 2 o resultado é 2
+ console.log(b<=a || b / 2 == 2)
+
+// verificar se a é maior que b e b maior ou igual a 5
+console.log(a>b && 5 < = b);
+
+// verificar se b é par ou ímpar usando ternario
+resultado = b % 2 == 0 ? "Par" : "Ímpar"
+
+/// pp
