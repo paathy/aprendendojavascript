de828c4f0d13c38d5652e5b9f4856f05fc95f74b
paathy <pathy.limaap@gmail.com>
2022-11-17 17:21:35 -0300

    aprendendo e testando dados e operadores javascript commit

---
+++ teste.js
@@ -0,0 +1,13 @@
+console.log("testando");
+var h = 10;
+var p = 20;
+var total = h + p;
+console.log(total);
+
+var nomePessoa = "Pathy";
+console.log(nomePessoa);
+
+
+var n = 19;
+console.log(typeof(n)); 
+
