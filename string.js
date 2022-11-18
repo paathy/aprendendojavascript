647d431b41e4dd548c962015aa71dd5b0d7b91de
paathy <pathy.limaap@gmail.com>
2022-11-18 00:00:31 -0300

    string commit

---
+++ string.js
@@ -0,0 +1,8 @@
+var nome = "Patrícia Lima";
+var x = nome.length;
+console.log(x);
+
+resultado = x > 15 ? "Obrigada" : "Por favor preencha seu nome completo";
+
+console.log(resultado);
+
--- teste.js
+++ teste.js
@@ -46,4 +46,3 @@ console.log(a>b && 5 < = b);
 // verificar se b é par ou ímpar usando ternario
 resultado = b % 2 == 0 ? "Par" : "Ímpar"
 
-/// pp
